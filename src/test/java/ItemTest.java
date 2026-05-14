d0b8e459f3b6c4bba48edc179efdc8c720540315
Lucy <merchanlucy39@gmail.com>
2026-05-14 15:05:33 -0400

    Update ItemTest

--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -25,39 +25,6 @@ public class ItemTest {
     }
 
     @Test
-    @DisplayName("borrowing a book changes the item status to borrowed")
-    public void borrowBookTest1() {
-        Student student = new Student("Lucy");
-        Book book = new Book("Frankenstein", "1234567891234", "Mary Shelley", "Science Fiction");
-
-        student.borrowItem(book);
-
-        Assertions.assertEquals(ItemStatus.BORROWED, book.getStatus());
-    }
-
-    @Test
-    @DisplayName("A borrowed book is not available")
-    public void borrowedBookTest2() {
-        Student student = new Student("Lucy");
-        Book book = new Book("Frankenstein", "1234567891234", "Mary Shelley", "Science Fiction");
-
-        student.borrowItem(book);
-
-        Assertions.assertFalse(book.isItemAvailable());
-    }
-
-    @Test
-    @DisplayName("Returning a borrowed book changes the item status to in store.")
-    public void returnBookTest1() {
-        Student student = new Student("Lucy");
-        Book book = new Book("Frankenstein", "1234567891234", "Mary Shelley", "Science Fiction");
-        student.borrowItem(book);
-        student.returnItem(book);
-
-        Assertions.assertEquals(ItemStatus.IN_STORE, book.getStatus());
-    }
-
-    @Test
     @DisplayName("marking a book as lost changes the item status to lost")
     public void markBookAsLostTest1() {
         Book book = new Book("Frankenstein", "1234567891234", "Mary Shelley", "Science Fiction");
